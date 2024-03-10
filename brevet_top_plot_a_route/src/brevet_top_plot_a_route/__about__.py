2963192f9cf8cf9e8a8aa73357d6422d6b2fbb85
Grigorii Batalov <grisxa@gmail.com>
2024-03-11 00:08:29 +0400

    Export CheckPoint class

--- brevet_top_plot_a_route/src/brevet_top_plot_a_route/__about__.py
+++ brevet_top_plot_a_route/src/brevet_top_plot_a_route/__about__.py
@@ -1,4 +1,4 @@
-__version__ = '0.1.3'
+__version__ = '0.1.4'
 __author__ = 'Grigorii Batalov'
 __license__ = 'MIT'
 __description__ = 'Utils for Plot-a-route service in the brevet.top'
--- brevet_top_plot_a_route/src/brevet_top_plot_a_route/__init__.py
+++ brevet_top_plot_a_route/src/brevet_top_plot_a_route/__init__.py
@@ -1,3 +1,4 @@
+from .check_point import CheckPoint  # noqa: F401
 from .route_point import RoutePoint  # noqa: F401
 from .main import get_route_info  # noqa: F401
 from .utils import ROUTE_PREFIX  # noqa: F401
