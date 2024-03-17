0282de0ac8a3d653a810a544081ad3e133eeefa8
Grigorii Batalov <grisxa@gmail.com>
2024-03-17 13:48:19 +0400

    Export of the resolve_document()

--- brevet_top_gcp_utils/src/brevet_top_gcp_utils/__init__.py
+++ brevet_top_gcp_utils/src/brevet_top_gcp_utils/__init__.py
@@ -1,1 +1,1 @@
-from .main import firestore_to_track_point, route_point_to_firestore, create_document  # noqa: F401
+from .main import firestore_to_track_point, route_point_to_firestore, create_document, resolve_document  # noqa: F401
