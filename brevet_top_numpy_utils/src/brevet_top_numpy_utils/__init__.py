e74b1861424de42730d7ddbd3a9626d94c279825
Grigorii Batalov <grisxa@gmail.com>
2024-05-09 23:08:04 +0400

    Distance functions updated

--- brevet_top_numpy_utils/src/brevet_top_numpy_utils/__init__.py
+++ brevet_top_numpy_utils/src/brevet_top_numpy_utils/__init__.py
@@ -1,2 +1,2 @@
 from .float_array import FloatArray  # noqa: F401
-from .main import np_geo_distance  # noqa: F401
+from .main import DISTANCE_FACTOR, np_geo_distance, np_geo_distance_track  # noqa: F401
