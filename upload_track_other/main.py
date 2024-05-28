d29c0e9f8aced099989dcc765ecefb142b624408
Grigorii Batalov <grisxa@gmail.com>
2024-05-29 01:18:28 +0400

    Other track uploading: checkpoint list only

--- upload_track_other/main.py
+++ upload_track_other/main.py
@@ -1,53 +1,41 @@
 import json
 import logging
 from base64 import b64decode
-from datetime import datetime
 
-import firebase_admin
 import google.cloud.logging
 import gpxpy
 import numpy as np
-from brevet_top_gcp_utils import (create_document, firestore_to_track_point,
-                                  get_checkpoints, resolve_document)
-from brevet_top_gcp_utils.auth_decorator import authenticated
 from brevet_top_numpy_utils import FloatArray
-from brevet_top_strava import (ActivityError, build_checkpoint_list,
-                               track_alignment, ActivityNotFound)
+from brevet_top_strava import (ActivityError, track_alignment, ActivityNotFound)
 from flask import Request
 from flask_cors import cross_origin
 from garmin_fit_sdk import Decoder, Stream, Profile
 from gpxpy.gpx import GPX
-from pytz import utc
+from more_itertools import flatten
 
 log_client = google.cloud.logging.Client()
 log_client.get_default_handler()
 log_client.setup_logging(log_level=logging.DEBUG)
 logging.basicConfig(level=logging.DEBUG)
 
-firebase_admin.initialize_app()
-db_client = google.cloud.firestore.Client()
-
 GARMIN_FIT_BASE = 11930465
+TRACK_DEVIATION_FACTOR: int = 600
+CONTROL_DEVIATION_FACTOR: int = 500
 
 
 @cross_origin(methods="POST")
-@authenticated
-def upload_track(request: Request, auth: dict):
+def upload_track_other(request: Request):
     """
     Compare the GPX track to a brevet route / checkpoints
 
     :param request: HTTP request
-    :param auth: exposed authorization header
     """
     logging.debug(f"Request {request}")
 
     data: dict = request.get_json().get("data", {})
 
-    # brevet.top document uid
-    brevet_uid: str | None = data.get("brevetUid")
-
-    # brevet.top rider uid
-    rider_uid: str | None = data.get("riderUid")
+    # checkpoints as a list of [latitude, longitude, distance, 0]
+    checkpoints = data.get("checkpoints", [])
 
     # GPX track as a text
     track: str = data.get("track", "")
@@ -55,8 +43,8 @@ def upload_track(request: Request, auth: dict):
     if not track:
         return json.dumps({"data": {"message": "No track given", "error": 400}}), 400
 
-    if not brevet_uid:
-        return json.dumps({"data": {"message": "Provide either a brevet UID", "error": 400}}), 400
+    if not checkpoints:
+        return json.dumps({"data": {"message": "Provide a checkpoint list", "error": 400}}), 400
 
     track_data = b64decode(track.removeprefix("data:application/octet-stream;base64,"))
     if track_data.startswith(b'<?xml'):
@@ -72,60 +60,26 @@ def upload_track(request: Request, auth: dict):
         return json.dumps({"data": {"message": "Empty track", "error": 400}}), 400
 
     try:
-        logging.info(f"Uploading track to brevet {brevet_uid} rider {rider_uid}")
-        if rider_uid != auth.get("uid"):
-            return (
-                json.dumps(
-                    {
-                        "data": {
-                            "message": f"Forbidden access to {rider_uid}",
-                            "error": 403,
-                        }
-                    }
-                ),
-                403,
-            )
+        distance = checkpoints[-1][2]
+        brevet = {
+            "short_track": checkpoints,
+            "trackDeviation": distance * TRACK_DEVIATION_FACTOR,
+            "controlDeviation": len(checkpoints) * CONTROL_DEVIATION_FACTOR,
+        }
+        copies = list(flatten([cp] * 2 for cp in checkpoints))
 
-        try:
-            brevet_doc = resolve_document(brevet_uid, db=db_client)
-        except ValueError as error:
-            return json.dumps({"data": {"message": str(error), "error": 404}}), 404
+        if len(checkpoints) == 1:
+            checkpoints = np.array(copies[0:-1], dtype=np.float64)
+        else:
+            checkpoints = np.array(copies[1:-1], dtype=np.float64)
+        points = track_alignment(brevet, draft, checkpoints)
 
-        brevet_dict = brevet_doc.get().to_dict()
-
-        # convert stored GeoPoints
-        brevet_dict["short_track"] = [
-            #  TODO: rename to shortTrack
-            firestore_to_track_point(p)
-            for p in brevet_dict.get("short_track", [])
-        ]
-
-        # prepare a list of control points (check-in / check-out) mandatory to visit
-        cps = get_checkpoints(brevet_dict["uid"], db=db_client)
-        checkpoints, ids = build_checkpoint_list(cps)
-
-        # the main alignment routine
-        points = track_alignment(brevet_dict, draft, checkpoints)
-        logging.info(f"{len(points)} points found")
-
-        # register check-ins / check-outs
-        for i, cp in enumerate(points):
-            # test for NaN
-            if cp[2] == cp[2]:
-                create_rider_barcode(
-                    data["riderUid"],
-                    code=ids[i],
-                    time=datetime.fromtimestamp(int(cp[2]), tz=utc),
-                )
-
-        return json.dumps({"data": {"message": len(points)}}), 200
-
+        return json.dumps({"data": {"message": points}}), 200
     except (ActivityError, ActivityNotFound) as error:
         return json.dumps({"data": {"message": str(error), "error": 400}}), 200
     except Exception as error:
         logging.exception(error)
         return json.dumps({"data": {"message": str(error), "error": 500}}), 500
-
 
 def build_array_from_gpx(data: GPX) -> FloatArray:
     """
@@ -174,17 +128,3 @@ def build_array_from_fit(data: bytes) -> FloatArray:
 
     decoder.read(mesg_listener=mesg_listener)
     return np.concatenate((draft, track_points), axis=0)
-
-
-def create_rider_barcode(rider_uid: str, code: str, time: datetime) -> str:
-    return create_document(
-        f"riders/{rider_uid}/barcodes",
-        {
-            "code": code,
-            "control": rider_uid,
-            "owner": rider_uid,
-            "time": time,
-            "message": "new",
-        },
-        db=db_client,
-    )
