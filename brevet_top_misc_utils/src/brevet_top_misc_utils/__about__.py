f2904417598a16f30d0f38a37fb4ec80800a02e9
Grigory Batalov <grisxa@gmail.com>
2025-07-02 23:51:40 +0400

    Common functions extracted

---
+++ brevet_top_misc_utils/src/brevet_top_misc_utils/__about__.py
@@ -0,0 +1,4 @@
+__version__ = "0.1.0"
+__author__ = "Grigorii Batalov"
+__license__ = "MIT"
+__description__ = "Misc utils for the brevet.top"
---
+++ brevet_top_misc_utils/src/brevet_top_misc_utils/__init__.py
@@ -0,0 +1,1 @@
+from .main import get_control_window, get_limit_hours  # noqa: F401
