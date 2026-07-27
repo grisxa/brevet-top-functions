22f90b31f0b14db44615806555aa6158981e66c9
Grigory Batalov <grisxa@gmail.com>
2026-07-27 21:56:58 +0400

    fix: Package rebuild (#23)
    * fix: Package rebuild

--- brevet_top_gcp_utils/src/brevet_top_gcp_utils/__about__.py
+++ brevet_top_gcp_utils/src/brevet_top_gcp_utils/__about__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.4"
+__version__ = "0.1.5"
 __author__ = "Grigorii Batalov"
 __license__ = "MIT"
 __description__ = "Google cloud utils for the brevet.top"
--- brevet_top_gcp_utils/src/brevet_top_gcp_utils/__init__.py
+++ brevet_top_gcp_utils/src/brevet_top_gcp_utils/__init__.py
@@ -1,2 +1,7 @@
-from .main import (create_document, firestore_to_track_point,  # noqa: F401
-                   get_checkpoints, resolve_document, route_point_to_firestore)  # noqa: F401
+from brevet_top_gcp_utils.main import (  # noqa: F401
+    create_document,
+    firestore_to_track_point,
+    get_checkpoints,
+    resolve_document,
+    route_point_to_firestore,
+)
